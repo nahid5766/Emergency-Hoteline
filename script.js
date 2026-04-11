8b38ead1f634f85f9da8a935b6b21101fead74fa
Md Nahidur Rahman <mdnahidurrahman@Mds-MacBook-Air.local>
2026-04-12 02:16:20 +0600

    love btn counter

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ let menu_area = document.getElementById("menu_area");
 menu_button.addEventListener("click", function() {
      menu_area.classList.toggle("hidden");
 })
+
+
+
+// Love btn counter (Function)
+let count = 0;
+
+const countDisplay = document.getElementById('favorite-count');
+
+const heartButtons = document.querySelectorAll('.heart-btn');
+
+heartButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        count++;
+        
+        countDisplay.textContent = count;
+
+        this.classList.toggle('active');
+
+        console.log(`Favorites updated: ${count}`);
+    });
+});
+
